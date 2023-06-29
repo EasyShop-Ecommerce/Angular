6a0e12d9aef1e7d5b1f1177e9d2fb36967fd2e0f
Eman Magdi <eman.magdi.akl@gmail.com>
2023-06-29 14:30:50 +0300

    cart

--- src/app/_Models/cart.ts
+++ src/app/_Models/cart.ts
@@ -1,7 +1,10 @@
 export interface cart {
-  productId: number;
-  productName: string;
-  price: number;
-  Quantity: number;
-  Image: string;
+  productId?: number;
+  productName?: string;
+  brandName?: string;
+  price?: number;
+  Quantity?: number;
+  Image?: string;
+  sellerId?:number;
+  shipPrice?:number
 }
